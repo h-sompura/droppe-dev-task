02344fc9d316110e5cd65282531a48473d5bba3a
Harshitha Sompura <sompharshi123@gmail.com>
2023-02-12 18:03:54 -0500

    Fix on click (#3)
    * refactor: simplified onClick function for Button in shop-app
    
    * refactor: simplified onClick function for the div in shop-app

--- src/shop-app.tsx
+++ src/shop-app.tsx
@@ -148,13 +148,7 @@ export class ShopApp extends React.Component<{}, State> {
         >
           <div className={styles.buttonWrapper}>
             <span role="button">
-              <Button
-                onClick={function (this: any) {
-                  this.setState({
-                    isOpen: true,
-                  });
-                }.bind(this)}
-              >
+              <Button onClick={() => this.setState({ isOpen: true })}>
                 Send product proposal
               </Button>
             </span>
@@ -187,11 +181,7 @@ export class ShopApp extends React.Component<{}, State> {
             <div className={styles.modalContentHelper}>
               <div
                 className={styles.modalClose}
-                onClick={function (this: any) {
-                  this.setState({
-                    isOpen: false,
-                  });
-                }.bind(this)}
+                onClick={() => this.setState({ isOpen: false })}
               >
                 <FaTimes />
               </div>
