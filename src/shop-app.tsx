339392da73697d6b2fde8e28d70e4e0a04a7f453
h-sompura <sompharshi123@gmail.com>
2023-02-12 19:28:01 -0500

    refactor: extracted Product type into a model file

--- src/shop-app.tsx
+++ src/shop-app.tsx
@@ -5,17 +5,11 @@ import { FaTimes } from "react-icons/fa";
 import { Button } from "./components/button";
 import ProductList from "./components/product-list-components";
 import { Form } from "./components/form";
+import { Product } from "./model/Product";
 import logo from "./images/droppe-logo.png";
 import img1 from "./images/img1.png";
 import img2 from "./images/img2.png";
 import styles from "./shopApp.module.css";
-
-type Product = {
-  title: string;
-  description: string;
-  price: string;
-  isFavorite?: boolean;
-};
 
 type State = {
   products: Product[];
